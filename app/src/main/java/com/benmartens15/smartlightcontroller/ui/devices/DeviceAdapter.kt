978da60d29755cef9fa84369589601bb0ae1bf32
Ben Martens <2benmartens15@gmail.com>
2023-08-07 10:18:10 -0400

    Can scan and show results in Devices Tab

--- app/src/main/java/com/benmartens15/smartlightcontroller/ui/devices/DeviceAdapter.kt
+++ app/src/main/java/com/benmartens15/smartlightcontroller/ui/devices/DeviceAdapter.kt
@@ -1,5 +1,7 @@
 package com.benmartens15.smartlightcontroller.ui.devices
 
+import android.annotation.SuppressLint
+import android.bluetooth.le.ScanResult
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,11 +9,17 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.benmartens15.smartlightcontroller.R
 
-class DeviceAdapter(private val dataList: List<Pair<String, String>>) :
+@SuppressLint("MissingPermission") // probably figure out how to handle this properly at some point
+class DeviceAdapter(private val items: List<ScanResult>) :
     RecyclerView.Adapter<DeviceAdapter.DeviceViewHolder>() {
     class DeviceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.text_view_device_name)
         val type: TextView = itemView.findViewById(R.id.text_view_device_type)
+
+        fun bind(result: ScanResult) {
+            name.text = result.device.name ?: "Unnamed"
+            type.text = result.device.address
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder {
@@ -21,12 +29,11 @@ class DeviceAdapter(private val dataList: List<Pair<String, String>>) :
     }
 
     override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
-        val (name, type) = dataList[position]
-        holder.name.text = name
-        holder.type.text = type
+        val item = items[position]
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return items.size
     }
 }
--- app/src/main/java/com/benmartens15/smartlightcontroller/ui/devices/DevicesFragment.kt
+++ app/src/main/java/com/benmartens15/smartlightcontroller/ui/devices/DevicesFragment.kt
@@ -1,36 +1,181 @@
 package com.benmartens15.smartlightcontroller.ui.devices
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.benmartens15.smartlightcontroller.R
 
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val REQUEST_FINE_LOCATION_PERMISSION = 2
+
+@SuppressLint("MissingPermission") // probably figure out how to handle this properly at some point
 class DevicesFragment : Fragment() {
 
-    private val dataList = mutableListOf<Pair<String, String>>()
+    private lateinit var recyclerView: RecyclerView
 
+    private val scanResults = mutableListOf<ScanResult>()
+    private val scanResultAdapter: DeviceAdapter by lazy {
+        DeviceAdapter(scanResults)
+    }
+
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = activity?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val scanSettings =
+        ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build()
+
+    private var isScanning = false
+
+    /*******************************************
+     * Activity function overrides
+     *******************************************/
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_devices, container, false)
-
-        dataList.clear()
-
-        dataList.add("All" to "")
-        dataList.add("Desk Backlight" to "RGB Strip")
-        dataList.add("Main Light" to "Light Switch")
-
-        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view_devices)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        val adapter = DeviceAdapter(dataList)
-        recyclerView.adapter = adapter
+        recyclerView = view.findViewById(R.id.recycler_view_devices)
 
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+        }
+        startBleScan()
+        setupRecyclerView()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ENABLE_BLUETOOTH_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK) { // if the user declined enabling bluetooth
+                promptEnableBluetooth()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_FINE_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                startBleScan() // all permissions granted - start BLE scan
+            } else {
+                // Handle the case where some or all permissions were denied by the user
+            }
+        }
+    }
+
+    /*******************************************
+     * Private functions
+     *******************************************/
+    private fun setupRecyclerView() {
+        recyclerView.apply {
+            adapter = scanResultAdapter
+            layoutManager = LinearLayoutManager(
+                context,
+                RecyclerView.VERTICAL,
+                false
+            )
+        }
+    }
+
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (context?.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            startActivityForResult(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+
+    private fun startBleScan() {
+        if (checkPermissions()) {
+            requestPermissions()
+        } else {
+            scanResults.clear()
+            scanResultAdapter.notifyDataSetChanged()
+            isScanning = true
+            bleScanner.startScan(null, scanSettings, scanCallback)
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val coarseLocationPermission =
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+
+            val fineLocationPermission =
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+
+            coarseLocationPermission && fineLocationPermission
+        } else {
+            true // Prior to Android 6.0, permissions are granted at install time
+        }
+    }
+
+    private fun requestPermissions() {
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_FINE_LOCATION_PERMISSION
+        )
+    }
+
+    /*******************************************
+     * Callback bodies
+     *******************************************/
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val indexQuery = scanResults.indexOfFirst { it.device.address == result.device.address }
+            if (indexQuery == -1) { // if there isn't already a scan result with the same address
+                with(result.device) {
+                    Log.i("ScanCallback", "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
+                }
+                scanResults.add(result)
+                scanResultAdapter.notifyItemInserted(scanResults.size - 1)
+            }
+        }
     }
 }
