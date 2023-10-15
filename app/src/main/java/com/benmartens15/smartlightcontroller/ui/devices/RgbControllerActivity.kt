3dac42dbc748d30663e82c76c26333daba1a5345
Ben Martens <2benmartens15@gmail.com>
2023-10-14 22:44:47 -0400

    Add color picker to RGB controller activity

--- app/src/main/java/com/benmartens15/smartlightcontroller/ui/devices/RgbControllerActivity.kt
+++ app/src/main/java/com/benmartens15/smartlightcontroller/ui/devices/RgbControllerActivity.kt
@@ -1,16 +1,45 @@
 package com.benmartens15.smartlightcontroller.ui.devices
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.benmartens15.smartlightcontroller.R
 import com.benmartens15.smartlightcontroller.databinding.ActivityRgbControllerBinding
+import com.benmartens15.smartlightcontroller.ui.ui.AppTheme
+import com.github.skydoves.colorpicker.compose.ColorEnvelope
+import com.github.skydoves.colorpicker.compose.HsvColorPicker
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
 
-class RgbControllerActivity : AppCompatActivity() {
+class RgbControllerActivity : ComponentActivity() {
     private lateinit var binding: ActivityRgbControllerBinding
     private val device by lazy {
         intent.getParcelableExtra<LightningLightController>("Device")
@@ -18,10 +47,22 @@ class RgbControllerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRgbControllerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setupUi()
+        setContent {
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    device?.displayName?.let {
+                        ColorPicker(
+                            it,
+                            Modifier
+                                .fillMaxWidth()
+                        )
+                    }
+                }
+            }
+        }
     }
 
     private fun setupUi() {
@@ -41,12 +82,12 @@ class RgbControllerActivity : AppCompatActivity() {
 
         val nameTextInputEditText: TextInputEditText =
             dialogLayout.findViewById(R.id.text_input_edit_text_device_name)
-        nameTextInputEditText.setText(binding.textViewDeviceName.text.toString())
+        nameTextInputEditText.setText(device?.displayName)
         nameTextInputEditText.selectAll()
 
         builder.setPositiveButton("OK") { dialog, _ ->
             val newName = nameTextInputEditText.text.toString()
-            binding.textViewDeviceName.text = newName
+//            binding.textViewDeviceName.text = newName
             device?.setName(newName)
             dialog.dismiss()
         }
@@ -69,5 +110,65 @@ class RgbControllerActivity : AppCompatActivity() {
         }, 200) // Delay in milliseconds
 
         dialog.show()
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ColorPicker(deviceName: String, modifier: Modifier = Modifier) {
+        val controller = rememberColorPickerController()
+        var name by remember {
+            mutableStateOf(deviceName)
+        }
+        var hexColorInput by remember {
+            mutableStateOf("FFFFFF")
+        }
+        var textClickEnabled by remember {
+            mutableStateOf(true)
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopAppBar(title = {
+                Text("RGB Controller")
+            })
+
+            Text(
+                modifier = modifier.padding(0.dp, 15.dp, 0.dp, 0.dp)
+                    .clickable(enabled = textClickEnabled) {
+                        textClickEnabled = false
+                        showChangeNameDialog()
+                                                           },
+                text = name,
+                textAlign = TextAlign.Center,
+                fontSize = 36.sp,
+            )
+
+            HsvColorPicker(
+                modifier = modifier.height(375.dp).padding(20.dp),
+                controller = controller,
+                onColorChanged = { colorEnvelope: ColorEnvelope ->
+                    Log.d("RgbControllerActivity", colorEnvelope.hexCode)
+                    hexColorInput = colorEnvelope.hexCode.substring(2)
+                }
+            )
+
+            OutlinedTextField(
+                value = hexColorInput,
+                onValueChange = {
+                    hexColorInput = it
+                },
+                label = { Text("Hex Code") }
+            )
+
+            Button(
+                modifier = modifier.padding(20.dp),
+                onClick = {
+                    device?.setRGBColour(hexColorInput)
+                }
+            ) {
+                Text("Set")
+            }
+        }
     }
 }
